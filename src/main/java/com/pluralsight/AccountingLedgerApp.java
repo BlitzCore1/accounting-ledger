c68eed6e5dba9e57db97592ccfbc54ca29e688bb
damarriusthompson <dthompson1@my.yearupunited.org>
2026-04-27 16:07:50 -0700

    Added home screen and updated the Transaction.java

--- src/main/java/com/pluralsight/AccountingLedgerApp.java
+++ src/main/java/com/pluralsight/AccountingLedgerApp.java
@@ -1,6 +1,52 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class AccountingLedgerApp
 {
-    static void main() {}
+    static Scanner scanner = new Scanner(System.in);
+
+    static void main()
+    {
+        displayHomeScreen();
+    }
+
+    static void displayHomeScreen()
+    {
+        System.out.println("Welcome to the Accounting Ledger App!");
+        System.out.println("-------------------------------------");
+        System.out.println();
+        System.out.println("D) Make a Deposit");
+        System.out.println("P) Make a Payment");
+        System.out.println("L) View Ledger");
+        System.out.println("X) Exit");
+        System.out.print("Make a selection: ");
+
+        String choice = scanner.nextLine().toUpperCase().strip();
+
+        System.out.println();
+        switch (choice)
+        {
+            case "D":
+                System.out.println("Create the Deposit Screen");
+                break;
+            case "P":
+                System.out.println("Create the Payment Screen");
+                break;
+            case "L":
+                System.out.println("Create the Ledger Screen");
+                break;
+            case "X":
+                System.out.println("Goodbye!");
+                // return exits the loop and the method, which in this case ends the application
+                return;
+            default:
+                System.out.println("Invalid selection. Please try again.");
+        }
+
+
+
+    }
+
+
 }
